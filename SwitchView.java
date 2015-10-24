0676ddfb5320a7b106d3d52e169ed5c7a74c2423
peng <peng@fio>
2015-10-24 14:19:16 +0800

    ..

--- SwitchView.java
+++ SwitchView.java
@@ -8,7 +8,6 @@ import android.graphics.RadialGradient;
 import android.graphics.RectF;
 import android.graphics.Shader;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.AccelerateInterpolator;
